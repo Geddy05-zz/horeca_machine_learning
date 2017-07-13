8dbcc378df9a68ebe5ffd451fd72a2f94a0d8921
Geddy Schellevis <geddyschellevis@gmail.com>
2017-07-13 14:14:19 +0200

    add bootstrap basic

--- flaskAlgorithms.py
+++ flaskAlgorithms.py
@@ -9,29 +9,17 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    data = get_date()
-    hw = HoltWinters(data["sales"])
-
-    result = hw.triple_exponential_smoothing(7, 0.8, 0.3, 0.9, 30)[-30:]
-    best_error = validater(result, hw.test)
-    return render_template('index.html', error=best_error)
-
-
-def get_date():
-    data = pd.read_csv('data/data.csv')
-    data["date"] = pd.to_datetime(data["date"])
-    return data
+    # data = get_date()
+    # hw = HoltWinters(data["sales"])
+    #
+    # result = hw.triple_exponential_smoothing(7, 0.8, 0.3, 0.9, 30)[-30:]
+    # best_error = validater(result, hw.test)
+    return render_template('index.html', error=5)
 
 
 def validater(result, test):
     # The mean squared error
     return np.mean((result - test) ** 2)
-
-
-def get_date():
-    data = pd.read_csv('/data/data.csv')
-    data["date"] = pd.to_datetime(data["date"])
-    return data
 
 
 if __name__ == '__main__':
